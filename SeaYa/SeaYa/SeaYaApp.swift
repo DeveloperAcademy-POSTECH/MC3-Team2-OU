36a0672d8e86672ff7ec63e20de166693e95ee69
chamroro <chamsae@handong.ac.kr>
2023-07-12 10:12:47 +0900

    💄 style : 메인뷰 전 뷰들

--- SeaYa/SeaYa/SeaYaApp.swift
+++ SeaYa/SeaYa/SeaYaApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct SeaYaApp: App {
+    @StateObject private var userData = UserData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(userData)
         }
     }
 }
