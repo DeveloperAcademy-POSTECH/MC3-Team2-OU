cb3d8b9ab879357c5b2b7bcca7dd92a9f3d84a54
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-07-14 15:44:49 +0900

    ✨ feat : 선택한 모든 날짜에 대한 모두의 가용시간 계산 및 정렬

--- SeaYa/SeaYa/Service/CalcOksService.swift
+++ SeaYa/SeaYa/Service/CalcOksService.swift
@@ -7,17 +7,17 @@ class CalcOksService: ObservableObject {
     @Published var theTime = 1
     
     @MainActor
-    func performCalculation(_ dateMembers : [DateMember]) async throws {
+    func performCalculation(_ dateMembers : [DateMember], by boundedDate : [BoundedDate]) async throws {
         isCalculating = true
         
         //try await
-        //Task.sleep(nanoseconds: 1_000_000_000)  // Simulating an asynchronous operation
-        let sum = try await getAvailableTime(dateMembers)
+        //Task.sleep(nanoseconds: 1_000_000_000)  //Simulating an asynchronous operation
+        let sum = try await getAvailableTime(dateMembers, by: boundedDate)
         result = sum
         isCalculating = false
     }
     
-    private func getAvailableTime(_ members : [DateMember]) async throws -> [TimeOks] {
+    private func getAvailableTime(_ members : [DateMember], by boundedDates : [BoundedDate]) async throws -> [TimeOks] {
         // print(members[0].Activities[0].startDate)
         
         // [start, length] -> [start, start+1, start+2, .... start+length-1]
@@ -25,6 +25,12 @@ class CalcOksService: ObservableObject {
             let startTime = Int(round(activity.startDate.timeIntervalSince1970/1800))
             let period = Int(round(activity.endDate.timeIntervalSince1970/1800)) - startTime
             return Array(stride(from:startTime, to: startTime + period, by : 1))}
+        
+        func getStride(_ activity : BoundedDate) -> [Int]{
+            let startTime = Int(round(activity.start.timeIntervalSince1970/1800))
+            let period = Int(round(activity.end.timeIntervalSince1970/1800)) - startTime
+            return Array(stride(from:startTime, to: startTime + period, by : 1))}
+        
         
         func getOktimes(_ member: DateMember) -> [Int]{
             let memberOkTimeMapped = member.dateEvents.map(getStride).flatMap{$0}
@@ -53,6 +59,14 @@ class CalcOksService: ObservableObject {
                     arr1.append(TimeOks(timeInt: a1[i].timeInt, Oks: aaron))}}
             return arr1}
         
+        func getOKtimesBySubtract(_ timeInt : Int, using dictMemberTimeOKs : [Int : Int]) -> TimeOks {
+            if let Oks =  dictMemberTimeOKs[timeInt] {
+                return TimeOks(timeInt: timeInt, Oks: members.count - Oks)
+            } else {
+                return TimeOks(timeInt: timeInt, Oks: members.count)
+            }
+        }
+        
         //시간 길이만큼 filteringAndMapping
         let membersOkTimeMapped = members.map(getOktimes).flatMap{$0}
 
@@ -61,19 +75,24 @@ class CalcOksService: ObservableObject {
             counts[number, default: 0] += 1}
         
         // startDate endDate 범위 정보 들어올 경우 해당 Date 범위만큼 [TimeOks] 배열 형성, 상단 countMembersOkTime 에서 해당 배열을 뺄셈하기
+        let boundedDatesMapped = boundedDates.map(getStride).flatMap{$0}
+        let nunnuna = boundedDatesMapped.map{getOKtimesBySubtract($0, using: countMembersOkTime)}.sorted(by: Nearest)
         
         //Dict [날짜절댓값/1800 : 가용인원] -> [구조체], 구조체(TimeOks){날짜 절댓값(timeInt) : Int , 가용인원(Oks) : Int}
         let memberTimeOks = countMembersOkTime.map{TimeOks(timeInt: $0.key, Oks: $0.value)}
         
         //주어진 시간만큼 필터링하기
-        let memberMinTimeOks = getMemberTimeOks(memberTimeOks, self.theTime)
+//        let memberMinTimeOks = getMemberTimeOks(memberTimeOks, self.theTime)
+        let memberMinTimeOks = getMemberTimeOks(nunnuna, self.theTime)
         
         //정렬하기
         let sortedMemberOks = memberMinTimeOks.sorted(by: OksAndNearest)
         
-        //그냥 Print문 확인차 출력
+        /// You can check values by using below commands
+        ///
         ///for TimeOks in sortedMemberOks{
         ///    let time = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: Date(timeIntervalSince1970: TimeInterval(TimeOks.timeInt*1800)))
         ///    print(time.year!, time.month!, time.day!, time.hour!, time.minute!, TimeOks.Oks)}
+        ///print(sortedMemberOks.last?.timeInt)
         return sortedMemberOks}
 }
