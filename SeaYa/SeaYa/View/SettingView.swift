dec721b5d0d864639ed7adc6c1839fb1618558cb
GaOn <xnoag@icloud.com>
2023-07-17 10:26:51 +0900

    ✨Feat: HapticsManager 함수 생성 #15

---
+++ SeaYa/SeaYa/View/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  SeaYa
+//
+//  Created by xnoag on 2023/07/17.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
