c40d30d2cb9c9774517aa74b7137c5326289f42a
chamroro <chamsae@handong.ac.kr>
2023-07-19 20:53:53 +0900

    💄 style : 카드 뷰 띄우기 위한 백그라운드 예시 코드

---
+++ SeaYa/SeaYa/Utils/CardBackgroundView.swift
@@ -0,0 +1,47 @@
+//
+//  SwiftUIView.swift
+//  SeaYa
+//
+//  Created by 김하은 on 2023/07/19.
+//
+import SwiftUI
+
+struct CardBackgroundView: View {
+    @State private var isCardVisible = false
+
+    var body: some View {
+        ZStack {
+            VStack {
+                // Main UI
+                Text("Main Content")
+                    .font(.largeTitle)
+
+                Button(action: {
+                    isCardVisible.toggle()
+                }) {
+                    Text("Show Card")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .blur(radius: isCardVisible ? 50 : 0)
+            
+            if isCardVisible {
+                Rectangle()
+                    .fill(Color(red: 0.45, green: 0.45, blue: 0.45).opacity(0.5))
+                    .ignoresSafeArea()
+                ResultView()
+                
+            }
+        }
+    }
+}
+
+struct CardBackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardBackgroundView()
+    }
+}
