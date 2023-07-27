a6e2894d950595fe1312f6c082f22ab7824ccc82
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-07-27 20:10:34 +0900

    ✨ feat : FixedTimeView 구현

--- SeaYa/SeaYa/View/FixedTimeView.swift
+++ SeaYa/SeaYa/View/FixedTimeView.swift
@@ -10,30 +10,87 @@ import SwiftUI
 struct FixedTimeView: View {
     @Binding var isFixedTimeSettingCompleted: Bool
     @State private var fixedTime = ""
+    @State var fixedTimeViewModel = FixedTimeViewModel()
+    @State var showSettingViewModal = false
+    @State var selectedElement = FixedTimeModel()
 
     var body: some View {
         VStack {
-            TextField("고정 시간을 입력하세요", text: $fixedTime)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-
-            Button(action: {
-                // 고정 시간 확인 로직 추가
-
-                if !fixedTime.isEmpty {
-                    isFixedTimeSettingCompleted = true
-                }
-            }) {
-                Text("확인")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
+            ForEach(fixedTimeViewModel.fixedTimeModels, id: \.self){ fixedTimeModel in
+                Button(
+                    action: {
+                        showSettingViewModal = true
+                        selectedElement = fixedTimeModel
+                    },
+                    label: {
+                       FixedTimeElementView(fixedTimeModel: fixedTimeModel)
+                   }
+                )
             }
+            
+            Button(
+                action: {
+                    showSettingViewModal = true
+                    selectedElement = FixedTimeModel()
+                    fixedTimeViewModel.fixedTimeModels.append(selectedElement)
+                }, label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundColor(Color.white)
+                        HStack{
+                            Text("고정일정 추가").body(textColor: .primaryColor)
+                            Spacer()
+                        }.padding(.leading, 18)
+                    }.frame(width: 358,height: 47)
+                }
+            )
+            
+
+            Button(
+                action: {
+                    if !fixedTime.isEmpty {
+                        isFixedTimeSettingCompleted = true
+                    }
+                },
+                label: {
+                    Text("확인")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                }
+            )
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .sheet(isPresented: $showSettingViewModal, content: {
+            EmptyView()
+                .presentationCornerRadius(32)
+        })
+        .background(Color.backgroundColor)
+    }
+}
+
+struct FixedTimeTestView : View{
+    @State var isFixedTimeSettingCompleted = false
+    @State var fixedTimeViewModel = FixedTimeViewModel([
+        FixedTimeModel(),
+        FixedTimeModel(),
+        FixedTimeModel()
+    ])
+    
+    var body: some View{
+        FixedTimeView(
+            isFixedTimeSettingCompleted: $isFixedTimeSettingCompleted,
+            fixedTimeViewModel: fixedTimeViewModel
+        )
+    }
+}
+
+struct FixedTimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        FixedTimeTestView()
     }
 }
