b320426f3a02751fbb8eda71291001035f678591
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-08-01 03:59:32 +0900

    ⚙️combine : CheckTimeDoneView 연동

--- SeaYa/SeaYa/View/CheckTimeDoneView.swift
+++ SeaYa/SeaYa/View/CheckTimeDoneView.swift
@@ -9,37 +9,52 @@ import SwiftUI
 
 struct CheckTimeDoneView: View {
     @ObservedObject var connectionManager: ConnectionService
+    @ObservedObject var calcOksManager = CalcOksService.shared
+    @State var period = 1
+    @State var ListUpViewResult : [[TimeOks]] = []
     
     var body: some View {
-        //Host
-        if !connectionManager.isHosting {
-            if connectionManager.listUP.count == connectionManager.peers.count{
-                //MARK: ListUpView Data 수정
-                ListUpView(timeOkGroup : [[TimeOks(timeInt: 940614, Oks: 2)],
-                                          [TimeOks(timeInt: 940620, Oks: 2),
-                                           TimeOks(timeInt: 940621, Oks: 2)],
-                                          [TimeOks(timeInt: 940615, Oks: 1),
-                                           TimeOks(timeInt: 940616, Oks: 1)],
-                                          [TimeOks(timeInt: 940622, Oks: 1),
-                                           TimeOks(timeInt: 940623, Oks: 1)],
-                                          [TimeOks(timeInt: 940625, Oks: 1),
-                                           TimeOks(timeInt: 940626, Oks: 1)]],
-                period: 1)
-                .onAppear() {
-                    connectionManager.send(true, messageType: .CheckTimeDone)
+        VStack{
+            //Host
+            if !connectionManager.isHosting {
+                if connectionManager.listUP.count == connectionManager.peers.count{
+                    //MARK: ListUpView Data 수정
+                    ListUpView(timeOkGroup : [[TimeOks(timeInt: 940614, Oks: 2)],
+                                              [TimeOks(timeInt: 940620, Oks: 2),
+                                               TimeOks(timeInt: 940621, Oks: 2)],
+                                              [TimeOks(timeInt: 940615, Oks: 1),
+                                               TimeOks(timeInt: 940616, Oks: 1)],
+                                              [TimeOks(timeInt: 940622, Oks: 1),
+                                               TimeOks(timeInt: 940623, Oks: 1)],
+                                              [TimeOks(timeInt: 940625, Oks: 1),
+                                               TimeOks(timeInt: 940626, Oks: 1)]],
+                               period: 1)
+                    //ListUpView(timeOkGroup: ListUpViewResult, period: period)
+                    .onAppear() {
+                        connectionManager.send(true, messageType: .CheckTimeDone)
+                    }
+                }else {
+                    WaitingForConfirmView()
                 }
-            }else {
-                WaitingForConfirmView()
             }
-        }
-        //Guest
-        else {
-            if connectionManager.isCheckTimeDone && connectionManager.scheduleDone == nil{
-                GuestWaitingForConfirmView()
-            }else if connectionManager.scheduleDone != nil {
-                ResultView(connectionManager: connectionManager)
-            }else {
-                WaitingForConfirmView()
+            //Guest
+            else {
+                if connectionManager.isCheckTimeDone && connectionManager.scheduleDone == nil{
+                    GuestWaitingForConfirmView()
+                }else if connectionManager.scheduleDone != nil {
+                    ResultView(connectionManager: connectionManager)
+                }else {
+                    WaitingForConfirmView()
+                }
+            }
+        }.onReceive(connectionManager.$listUP){ _ in
+            if connectionManager.listUP.count == connectionManager.peers.count && !connectionManager.isHosting {
+                Task{
+                    calcOksManager.theTime = period
+                    calcOksManager.dateMembers = connectionManager.listUP
+                    try await calcOksManager.performCalculation(connectionManager.listUP, by: [])
+                    ListUpViewResult = calcOksManager.groupByConsecutiveTime(calcOksManager.result!)
+                }
             }
         }
     }
