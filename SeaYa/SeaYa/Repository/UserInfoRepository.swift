5268239a978097c0b387ac452213d6e73c62dd24
musung <musungkim1352@gmail.com>
2023-07-31 10:06:18 +0900

    ✨feat: UserData 수정

--- SeaYa/SeaYa/Repository/UserInfoRepository.swift
+++ SeaYa/SeaYa/Repository/UserInfoRepository.swift
@@ -16,23 +16,50 @@ class UserInfoRepository{
          self.userDefaults = userDefaults
      }
     
-//    public func getUserInfo() -> UserData{
-//    }
-    public func createUserInfo(userInfo: UserData){
-        if let encoded = try? encoder.encode(userInfo) {
-            userDefaults.set(encoded, forKey: "userInfo")
-        }
+    public func setUid(uid: String){
+        let uid = UUID().uuidString
+        userDefaults.set(uid, forKey: "uid")
     }
-    public func updateUserInfo(userInfo: UserData){
-        createUserInfo(userInfo: userInfo)
-        
-    }
-    public func getUserInfo()->UserData?{
-        if let savedData = UserDefaults.standard.object(forKey: "userInfo") as? Data {
-            if let savedObject = try? decoder.decode(UserData.self, from: savedData) {
-                return savedObject
-            }
+    public func getUid() -> String?{
+        if let uid = UserDefaults.standard.object(forKey: "uid") as? String {
+            return uid
         }
         return nil
     }
+    public func setNickName(nickName: String){
+        userDefaults.set(nickName, forKey: "nickName")
+    }
+    public func getNickName() -> String?{
+        if let nickName = UserDefaults.standard.object(forKey: "nickName") as? String {
+            return nickName
+        }
+        return nil
+    }
+    public func setImageName(imageName: String){
+        userDefaults.set(imageName, forKey: "imageName")
+    }
+    public func getImageName() -> String?{
+        if let imageName = UserDefaults.standard.object(forKey: "imageName") as? String {
+            return imageName
+        }
+        return nil
+    }
+    
+//    public func createUserInfo(userInfo: UserData){
+//        if let encoded = try? encoder.encode(userInfo) {
+//            userDefaults.set(encoded, forKey: "userInfo")
+//        }
+//    }
+//    public func updateUserInfo(userInfo: UserData){
+//        createUserInfo(userInfo: userInfo)
+//
+//    }
+//    public func getUserInfo()->UserData?{
+//        if let savedData = UserDefaults.standard.object(forKey: "userInfo") as? Data {
+//            if let savedObject = try? decoder.decode(UserData.self, from: savedData) {
+//                return savedObject
+//            }
+//        }
+//        return nil
+//    }
 }
